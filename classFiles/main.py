5bda2c14f942c30560a558d14a95054d3ffa6f74
Vishal Sowrirajan <vishalsowrirajan5@gmail.com>
2021-01-30 18:22:02 +0100

    add comments

--- classFiles/main.py
+++ classFiles/main.py
@@ -12,17 +12,8 @@ data_parser = Preprocessor(dataset=dataset_name)
 features = data_parser.parse_dataset()
 
 X_train, X_test, y_train, y_test = train_test_split(features.iloc[:, :-1], features.iloc[:, -1], test_size=0.33)
-# TODO:
-# 1. Write a clean and structured code - train.py, evaluation.py - Done
-# 2. Start visualizing the Confusion matrix - Done
-# 3. Write the README file
-# 4. Create a git repo and push it  - Done
-# 5. Visualization for feature importance - bar graph - Done
-# 6. Visualize Segment wise shock levels
-# 7. Flags for multiple models and write in readme how to activate them
-# 8. Future scopes - predicting the shock timing, shock alert
-# 9. Why current approach: we can decide if this segment vehicle runs for so and so hours, what will be the level of shock (anticipate)
 
+# Model selection - we choose RANDOM FOREST as our classifier
 model = RandomForestClassifierModel(model_name=RandomForestClassifier())
 
 # Train the model and Save the weights
