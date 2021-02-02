1d726226c1f55033a53c0fe2dccf5988a2a3f80e
Vishal Sowrirajan <vishalsowrirajan5@gmail.com>
2021-02-02 09:13:08 +0100

    Update Piechart

--- Visualization/pie_chart.py
+++ Visualization/pie_chart.py
@@ -6,6 +6,6 @@ my_labels = 'Elektro. Gegengewichtsstapler','Hochhubwagen','Hochhubwagen- KMS', 
 my_explode = (0, 0, 0.1, 0.1, 0.1, 0.1, 0.1, 0)
 pie  = plt.pie(my_data, autopct='%1.1f%%', startangle=15, shadow = True, explode=my_explode)
 plt.legend(pie[0], my_labels, loc="lower right", bbox_to_anchor = (0.2,0.4))
-plt.title('My Tasks')
+plt.title('Segment-wise Shock counts')
 plt.axis('equal')
 plt.show()
